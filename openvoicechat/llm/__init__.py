71e19d55704f97f1622a7b057315a6e86f43bbda
invent-c <amirgulzar328@gmail.com>
2025-05-12 11:38:33 +0500

    final commit-v1

---
+++ openvoicechat/llm/__init__.py
@@ -0,0 +1,4 @@
+from .base import BaseChatbot as BaseChatbot
+from .llm_EC2 import Chatbot_LLM as Chatbot_LLM
+from .llm_llama import Chatbot_llama as Chatbot_llama
+# from .llm_hf import Chatbot as Chatbot_hf
